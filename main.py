4eecc58a6613cf8398f2bb7db22375eb27cacd21
Username387J <jdeleijagbulu@gmail.com>
2025-01-15 18:35:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ TITLE="BEE GAME"
 flower=Actor("flower")
 flower.pos=200,200
 
+bee=Actor("bee")
+bee.pos=100,300
+
+background=Actor("background")
+background.pos=200,200
+
+
+
 def draw():
     screen.fill("black")
     flower.draw()
+    bee.draw()
+    background.draw()
 
 pgzrun.go()
-    
